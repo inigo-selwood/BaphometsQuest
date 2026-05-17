d14442cd9d2692c8dd1b200845b6aad038161e72
inigo-selwood <inigo.selwood@outlook.com>
2026-05-17 22:59:51 +0100

    Remove legacy engine and scene architecture

--- source/engine/node/objectNode.cpp
+++
@@ -1,9 +0,0 @@
-#include "objectNode.hpp"
-
-namespace Engine {
-
-SDL_Point ObjectNode::getWorldPosition() const {
-    return this->getGlobalPosition();
-}
-
-} // namespace Engine
--- source/game/nodes/colourChangeBox.cpp
+++
@@ -1,9 +0,0 @@
-#include "colourChangeBox.hpp"
-
-void ColourChangeBox::registerType() {
-    Engine::Node::registerType<ColourChangeBox>("ColourChangeBox");
-}
-
-void ColourChangeBox::turnGreen() {
-    this->setProperty("colour", SDL_Color{64, 255, 112, 255});
-}
