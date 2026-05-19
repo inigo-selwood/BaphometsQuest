46601df3b45abdcc62e894328d4ed27e06dbb182
inigo-selwood <inigo.selwood@outlook.com>
2026-05-19 05:30:51 +0100

    Bundle and format scene XML

--- configuration/cmake/distribution.cmake
+++ configuration/cmake/distribution.cmake
@@ -7,11 +7,39 @@ function(configure_distribution targetName)
         ${CMAKE_SOURCE_DIR}/resources/*
     )
 
+    file(GLOB_RECURSE sceneResourceFiles CONFIGURE_DEPENDS
+        ${CMAKE_SOURCE_DIR}/source/scenes/*.xml
+    )
+
+    set(sceneResourceCommands)
+
+    foreach(sceneResourceFile ${sceneResourceFiles})
+        file(RELATIVE_PATH
+            sceneResourcePath
+            ${CMAKE_SOURCE_DIR}/source/scenes
+            ${sceneResourceFile}
+        )
+        get_filename_component(sceneResourceDirectory
+            ${sceneResourcePath}
+            DIRECTORY
+        )
+
+        list(APPEND sceneResourceCommands
+            COMMAND ${CMAKE_COMMAND} -E make_directory
+                $<TARGET_FILE_DIR:${targetName}>/source/scenes/${sceneResourceDirectory}
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                ${sceneResourceFile}
+                $<TARGET_FILE_DIR:${targetName}>/source/scenes/${sceneResourcePath}
+        )
+    endforeach()
+
     add_custom_target(bundle_resources
         COMMAND ${CMAKE_COMMAND} -E copy_directory
             ${CMAKE_SOURCE_DIR}/resources
             $<TARGET_FILE_DIR:${targetName}>/resources
+        ${sceneResourceCommands}
         DEPENDS ${resourceFiles}
+            ${sceneResourceFiles}
         COMMENT "Bundling resources"
     )
 
